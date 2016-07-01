e9d432834d69f716bf3783ddf22bef4b209277db
Ryan Kulp <ryanckulp@gmail.com>
2016-07-01 00:26:39 -0700

    browser action, description

--- icon.js
+++ icon.js
@@ -1,4 +1,4 @@
-// Called when user clicks on the Distribute icon 'browser action'
+// Called when user clicks the SJW icon 'browser action'
 chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.create({"url": "http://quote.distribute.com"});
+  chrome.tabs.create({"url": "https://github.com/ryanckulp/SJW_Bot_Extension"});
 });
